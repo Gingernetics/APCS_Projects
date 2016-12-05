8e80dcc7cba688e94dfb6c86aa8c52abdd60a9d3
Gingernetics <janstefan.kowalski@gmail.com>
2016-12-05 13:03:42 -0500

    THis is a working .compare file

--- InterfaceLab01/SkiClient.java
+++ InterfaceLab01/SkiClient.java
@@ -1,9 +1,43 @@
 public class SkiClient{
 
-	public static void main(String [] args){
-	SkiJumper a = new SkiJumper("Bob", "Ross");
+    // precondition: jumpers != null and jumpers.length > 0
+    // postcondition: returns the SkiJumper with the most jumps
+    public static SkiJumper mostExperienced(SkiJumper [] jumpers){
+	SkiJumper ans = jumpers[0];
+	for (SkiJumper s : jumpers)
+		if (s.compareTo(ans) > 0)
+			ans = s;
+	return ans;
+    }
+    
 
-	System.out.println(a);
-	
+    public static void main(String [] args){
+	SkiJumper[] jumpers = {new SkiJumper("Bill","White"),
+			       new SkiJumper("Carol", "Sage"),
+			       new SkiJumper("Pat", "Lee")
+	};
+
+	/*
+	// print jumpers
+	for (SkiJumper s : jumpers)
+	    System.out.println(s);
+	*/
+
+	// train
+	for (SkiJumper s : jumpers){
+	    s.train(Math.random() * 100);
+	    while (Math.random() < 0.9)  s.fly();
 	}
+
+	// print jumpers
+	for (SkiJumper s : jumpers)
+	    System.out.println(s);
+
+
+	// find the most experience SkiJumper
+	System.out.println("Most experienced jumper: " + mostExperienced(jumpers));
+	
+    }
+
+
 }
--- InterfaceLab01/SkiJumper.java
+++ InterfaceLab01/SkiJumper.java
@@ -1,28 +1,42 @@
-public class SkiJumper extends Athlete implements Flier{
-
-	private int _numofjumps;
-	
-	public SkiJumper (String first, String last){
-		super(first, last);
-		_numofjumps = 0;
-	}
-
-	public void fly(){
-		_numofjumps++;
-	}
-
-	public int getJumps(){
-		return _numofjumps;
-	}
-	
-	public void train(double time){
-		_hours += time;
-	}
-
-	public String toString(){
-		return super.toString() + " | Number of jumps: " + _numofjumps;
-	}
-	
+public class SkiJumper extends Athlete implements Flier, Comparable<SkiJumper>{
 
 
+    // attributes
+    private int _numJumps;
+
+    // constructor
+    public SkiJumper(String first, String last){
+	super(first, last);
+	_numJumps = 0;
+    }
+
+    // accessor methods
+    public int getJumps(){
+	return _numJumps;
+    }
+
+    // modifier methods 
+    public void fly(){
+	_numJumps++;
+    }
+
+    public void train(double session){
+	setHoursTrained(getHoursTrained() + session);
+
+    }
+
+    
+
+    // override
+    public String toString(){
+	String ans = super.toString();
+	ans += " # jumps: " + _numJumps;
+	return ans;
+    }
+
+    public int compareTo(SkiJumper other){
+	return this.getJumps() - other.getJumps();
+    }
+    
+    
 }
