a1c7d680b824f8bf8b4a2e6ac039b43c3f75a383
Gingernetics <janstefan.kowalski@gmail.com>
2017-01-04 13:09:39 -0500

    Hadamard

---
+++ Hadamard/Hadamard.java
@@ -0,0 +1,27 @@
+public class Hadamard{
+
+      	private boolean [][] _mat;
+	
+
+       	// constructor
+       	// precondition : n is a power of 2
+       	public Hadamard(int n){
+       		_mat = new Boolean[n][n];
+		if (n = 1)
+			_mat = {T};
+		
+		
+       	}
+
+       	// postcondition: prints the Hadamard matrix
+       	//                use T for true, F for false
+       	public String toString(){
+		for(int row = 0; row < 
+       	}
+
+	public static void main(String args[]){
+		System.out.println(Hadamard(args 0));
+	}
+
+
+}
