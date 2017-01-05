85f4e2ebb57579ec65ed481a8a382c5c88cfb177
Gingernetics <janstefan.kowalski@gmail.com>
2017-01-05 13:09:27 -0500

    Lists today

--- Hadamard/Hadamard.java
+++ Hadamard/Hadamard.java
@@ -1,26 +1,53 @@
 public class Hadamard{
 
-      	private boolean [][] _mat;
+      	private static boolean [][] _mat;
 	
 
        	// constructor
        	// precondition : n is a power of 2
-       	public Hadamard(int n){
-       		_mat = new Boolean[n][n];
-		if (n = 1)
-			_mat = {T};
-		
-		
+       	public static void Hadamard(int n){
+		int pow = (int) java.lang.Math.pow(2,n);
+       		_mat = new boolean[pow][pow];
+		filler(n, pow);		
        	}
 
        	// postcondition: prints the Hadamard matrix
        	//                use T for true, F for false
-       	public String toString(){
-		for(int row = 0; row < 
+       	//public String toString(){
+	public static void print(){
+		int n = _mat.length;
+		for(int row = 0; row < n; row++){
+			for(int col = 0; col < n; col++){
+				System.out.print(converter(_mat[row][col]) + " ");
+			}
+			System.out.println();
+		}
        	}
 
+	public static void filler(int n, int pow){
+		_mat[0][0] = true;
+		for (int i = 0; i <= n; i++){
+			int step = (int) java.lang.Math.pow(2,i);
+			for(int row = step; row < step + 1; row++){
+				for(int col = step; col < step + 1; col++){
+					_mat[row][col] = _mat[row - step][col - step];
+				}
+			}	
+		
+		}
+
+	}
+
+	public static String converter(boolean value){
+		if (value)
+			return "T";
+		else
+			return "F";
+	}
+
 	public static void main(String args[]){
-		System.out.println(Hadamard(args 0));
+		Hadamard(Integer.parseInt(args[0]));
+		print();
 	}
 
 
