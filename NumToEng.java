f297e8203de445d21da9e1e8b9d28687441ec928
Gingernetics <janstefan.kowalski@gmail.com>
2017-02-06 13:10:13 -0500

    Hello

---
+++ NumToEng.java
@@ -0,0 +1,53 @@
+public class NumToEng{
+
+    private String _eng;
+    private int _n;
+
+    public NumToEng(int n){
+	_n = n;
+	_eng = convert(n);
+    }
+
+    // precondition: 0 <= n < 10
+    public static String digits(int n){
+	String[] d = {"zero","one","two","three","four",
+		      "five", "six","seven", "eight", "nine"};
+	return d[n];
+    }
+
+ // precondition: 0 <= n < 10
+    public static String teens(int n){
+	String[] d = {"ten","eleven","twelve","thirteen","fourteen","fifteen",
+		      "sixteen", "seventeen","eighteen", "nineteen", "twenty"};
+	return d[n];
+    }
+    
+ // precondition: 0 <= n < 10
+    public static String tens(int n){
+	String[] d = {"", "", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"};
+	return d[n];
+    }
+
+    // precondition : 0 <= n < 10
+    public static String convert(int n){
+	if ( n < 10) return digits(n);
+	if (n < 20) return teens(n - 10 );
+	if (n % 10 == 0) return tens(n /10) ;
+	if (n < 100) return tens(n / 10) + "-" + convert(n % 10);
+	if (n < 100) return tens(n / 10) + "-" + convert(n % 10);
+	return "DONE";
+    }
+
+    public String toString(){
+	return _n + " : " + _eng;
+    }
+    
+
+    public static void main(String [] args){
+	int n = 100;
+	for (int i = 0; i < n; i++){
+	    System.out.println(new NumToEng(i));
+	}
+    }
+
+}
